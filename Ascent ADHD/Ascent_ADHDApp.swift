956616e5f1566b7c3c64fcff4e0a2d5c6872d17f
Sam Dalton <samueldalton@ridgelinecounselingid.com>
2026-07-16 09:56:54 -0600

    Initial Commit

---
+++ Ascent ADHD/Ascent_ADHDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ascent_ADHDApp.swift
+//  Ascent ADHD
+//
+//  Created by Karlie Dalton on 7/16/26.
+//
+
+import SwiftUI
+
+@main
+struct Ascent_ADHDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ascent ADHDTests/Ascent_ADHDTests.swift
@@ -0,0 +1,18 @@
+//
+//  Ascent_ADHDTests.swift
+//  Ascent ADHDTests
+//
+//  Created by Karlie Dalton on 7/16/26.
+//
+
+import Testing
+
+struct Ascent_ADHDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
